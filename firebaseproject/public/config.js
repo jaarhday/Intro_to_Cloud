6512c8be5c120be9029528718e7524b39284aec4
Alec Day <113727995+jaarhday@users.noreply.github.com>
2025-02-04 16:11:06 -0700

    firebase work 2/4/2025

---
+++ firebaseproject/public/config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC0SXfR9axLyC16XkI7Uue7opzgNTgBscM",
+  authDomain: "alecday-fa850.firebaseapp.com",
+  projectId: "alecday-fa850",
+  storageBucket: "alecday-fa850.firebasestorage.app",
+  messagingSenderId: "492339333537",
+  appId: "1:492339333537:web:ea9751e5810e89b97a63e0",
+  measurementId: "G-NKJM56MMSL"
+};
+
+
+  
